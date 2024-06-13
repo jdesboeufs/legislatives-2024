b80c45a44a477d1ede5c2e42b3c8187a12b6bf54
Jérôme Desboeufs <jerome@livingdata.co>
2024-06-13 23:34:30 +0200

    Improve read stream

---
+++ build-resultats-eu2024-bv-csv.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+import process from 'node:process'
+import {Transform} from 'node:stream'
+import Papa from 'papaparse'
+import {createReader} from './lib/eu2024-reader.js'
+
+const serialize = new Transform({
+  transform(bv, encoding, callback) {
+    for (const liste of bv.listes) {
+      this.push(Papa.unparse([{
+        codeDepartement: bv.codeDepartement,
+        nomDepartement: bv.nomDepartement,
+        codeCommune: bv.codeCommune,
+        nomCommune: bv.nomCommune,
+        codeBureauVote: bv.codeBureauVote,
+        codeCirconscription: bv.codeCirconscription,
+        nomCirconscription: bv.nomCirconscription,
+        inscrits: bv.inscrits,
+        votants: bv.votants,
+        pourcentageVotants: bv.pourcentageVotants,
+        abstentions: bv.abstentions,
+        pourcentageAbstentions: bv.pourcentageAbstentions,
+        exprimes: bv.exprimes,
+        pourcentageExprimesInscrits: bv.pourcentageExprimesInscrits,
+        pourcentageExprimesVotants: bv.pourcentageExprimesVotants,
+        blancs: bv.blancs,
+        pourcentageBlancsInscrits: bv.pourcentageBlancsInscrits,
+        pourcentageBlancsVotants: bv.pourcentageBlancsVotants,
+        nuls: bv.nuls,
+        pourcentageNulsInscrits: bv.pourcentageNulsInscrits,
+        pourcentageNulsVotants: bv.pourcentageNulsVotants,
+        nuanceListe: liste.nuanceListe,
+        libelleAbregeListe: liste.libelleAbregeListe,
+        libelleListe: liste.libelleListe,
+        voixListe: liste.voixListe,
+        pourcentageVoixInscritsListe: liste.pourcentageVoixInscritsListe,
+        pourcentageVoixExprimesListe: liste.pourcentageVoixExprimesListe
+      }], {header: !this.headerSent, newline: '\n', quotes: true}) + '\n')
+
+      this.headerSent = true
+    }
+
+    callback()
+  },
+  objectMode: true
+})
+
+const reader = await createReader()
+
+reader
+  .pipe(serialize)
+  .pipe(process.stdout)
