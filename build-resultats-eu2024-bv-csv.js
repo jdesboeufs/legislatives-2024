e481bc05da3dc1c929126c4e1aa6bfe65269a85a
Jérôme Desboeufs <jerome@livingdata.co>
2024-06-14 09:40:22 +0200

    Create csv formatter

--- build-resultats-eu2024-bv-csv.js
+++ build-resultats-eu2024-bv-csv.js
@@ -1,22 +1,10 @@
 #!/usr/bin/env node
 import process from 'node:process'
-import {Transform} from 'node:stream'
-import Papa from 'papaparse'
 import {createReader} from './lib/eu2024-reader.js'
-import {flattenListes} from './lib/flatten.js'
-
-const serialize = new Transform({
-  transform(bv, encoding, callback) {
-    const rows = flattenListes(bv)
-    this.push(Papa.unparse(rows, {header: !this.headerSent, newline: '\n', quotes: true}) + '\n')
-    this.headerSent = true
-    callback()
-  },
-  objectMode: true
-})
+import {createCsvFormatter} from './lib/formatters/csv.js'
 
 const reader = await createReader()
 
 reader
-  .pipe(serialize)
+  .pipe(createCsvFormatter())
   .pipe(process.stdout)
