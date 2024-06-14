7fbf45fb59fcc71b49b37e650e913969c3a5ea26
Jérôme Desboeufs <jerome@livingdata.co>
2024-06-14 14:07:19 +0200

    Drop legacy code

--- build-resultats-eu2024-legacy.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import {writeFile, mkdir} from 'node:fs/promises'
-import {computeResultats} from './lib/eur2024-legacy.js'
-
-await mkdir('dist', {recursive: true})
-
-const resultats = await computeResultats()
-await writeFile('dist/resultats-eu2024.json', JSON.stringify(resultats, null, 2))
