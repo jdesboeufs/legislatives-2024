fe20bd2a867488e55da6d2d6de54b7c50e77c34d
Jérôme Desboeufs <jerome@livingdata.co>
2024-06-14 09:34:53 +0200

    Use flattenListes

--- build-resultats-eu2024-bv-csv.js
+++ build-resultats-eu2024-bv-csv.js
@@ -3,43 +3,13 @@ import process from 'node:process'
 import {Transform} from 'node:stream'
 import Papa from 'papaparse'
 import {createReader} from './lib/eu2024-reader.js'
+import {flattenListes} from './lib/flatten.js'
 
 const serialize = new Transform({
   transform(bv, encoding, callback) {
-    for (const liste of bv.listes) {
-      this.push(Papa.unparse([{
-        codeDepartement: bv.codeDepartement,
-        nomDepartement: bv.nomDepartement,
-        codeCommune: bv.codeCommune,
-        nomCommune: bv.nomCommune,
-        codeBureauVote: bv.codeBureauVote,
-        codeCirconscription: bv.codeCirconscription,
-        nomCirconscription: bv.nomCirconscription,
-        inscrits: bv.inscrits,
-        votants: bv.votants,
-        pourcentageVotants: bv.pourcentageVotants,
-        abstentions: bv.abstentions,
-        pourcentageAbstentions: bv.pourcentageAbstentions,
-        exprimes: bv.exprimes,
-        pourcentageExprimesInscrits: bv.pourcentageExprimesInscrits,
-        pourcentageExprimesVotants: bv.pourcentageExprimesVotants,
-        blancs: bv.blancs,
-        pourcentageBlancsInscrits: bv.pourcentageBlancsInscrits,
-        pourcentageBlancsVotants: bv.pourcentageBlancsVotants,
-        nuls: bv.nuls,
-        pourcentageNulsInscrits: bv.pourcentageNulsInscrits,
-        pourcentageNulsVotants: bv.pourcentageNulsVotants,
-        nuanceListe: liste.nuance,
-        libelleAbregeListe: liste.libelleAbrege,
-        libelleListe: liste.libelle,
-        voixListe: liste.voix,
-        pourcentageVoixInscritsListe: liste.pourcentageVoixInscrits,
-        pourcentageVoixExprimesListe: liste.pourcentageVoixExprimes
-      }], {header: !this.headerSent, newline: '\n', quotes: true}) + '\n')
-
-      this.headerSent = true
-    }
-
+    const rows = flattenListes(bv)
+    this.push(Papa.unparse(rows, {header: !this.headerSent, newline: '\n', quotes: true}) + '\n')
+    this.headerSent = true
     callback()
   },
   objectMode: true
